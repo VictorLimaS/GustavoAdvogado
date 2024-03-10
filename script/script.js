dc2381f6d8e1f9ed702588b4d3af5fd5461840c1
Victor Lima <victorlimasantos99@hotmail.com>
2024-03-10 03:50:22 -0300

    Subindo

--- script/script.js
+++ script/script.js
@@ -1,9 +1,18 @@
+// DIRECIONANDO A OS LINKS COM OS BOTOES
+
 const linkBotaoFgts = document.getElementById('botao_fgts');
 const linkBotaoSalario = document.getElementById('botao_salario');
+const linkBotaoSocilInsta = document.getElementById('social1');
+const linkBotaoSocialWhats = document.getElementById('social2');
 const linkBotaoTrabalhos = document.querySelectorAll('#botao_trabalhos');
 
+linkBotaoSocilInsta.addEventListener('click', function () {
+    window.open('https://www.instagram.com/advgusttavonogueira/', '_blank')
+});
 
-
+linkBotaoSocialWhats.addEventListener('click', function () {
+    window.open('https://w.app/c4S3QE', '_blank')
+});
 
 linkBotaoFgts.addEventListener('click', function () {
     window.open('https://www.jusbrasil.com.br/artigos/multa-dos-40-do-fgts-no-contrato-de-trabalho-temporario/2133490920', '_blank')
@@ -19,6 +28,7 @@ linkBotaoTrabalhos.forEach(botao => {
     });
 });
 
+// SCROLL LENTO ATE A SECTION
 
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll("header nav ul li a");
@@ -41,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
+// LI FICAREM SELECIONADA COM O HOVER
 
 document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll("section");
@@ -68,15 +78,28 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", highlightNavItem);
 });
 
-
+// MENU hamburguer
 
 const hamburguer = document.querySelector('.hamburguer');
 const menuHamburguer = document.querySelector('.menu-hamburguer');
+const menuItems = document.querySelectorAll('.menu-hamburguer li a');
 
+// Evento de clique no ícone de hambúrguer
 hamburguer.addEventListener('click', () => {
     menuHamburguer.style.display = 'block';
 });
 
-menuHamburguer.addEventListener('click', () => {
-    menuHamburguer.style.display = 'none';
+// Evento de clique em qualquer parte do menu de hambúrguer
+menuHamburguer.addEventListener('click', (event) => {
+    // Verifica se o clique ocorreu em um item do menu
+    if (event.target.tagName === 'A') {
+        // Remove a classe ativa de todos os itens do menu
+        menuItems.forEach(item => {
+            item.classList.remove('active');
+        });
+        // Adiciona a classe ativa apenas ao item clicado
+        event.target.classList.add('active');
+        // Fecha o menu de hambúrguer
+        menuHamburguer.style.display = 'none';
+    }
 });
