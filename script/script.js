1de0698f20dab69764db7f8f1700811145e86dfb
Victor Lima <victorlimasantos99@hotmail.com>
2024-03-12 11:04:52 -0300

    Subindo

--- script/script.js
+++ script/script.js
@@ -1,30 +1,25 @@
 // DIRECIONANDO A OS LINKS COM OS BOTOES
 
-const linkBotaoFgts = document.getElementById('botao_fgts');
-const linkBotaoSalario = document.getElementById('botao_salario');
-const linkBotaoSocilInsta = document.getElementById('social1');
-const linkBotaoSocialWhats = document.getElementById('social2');
+const urls = [
+    { id: 'botao_fgts', url: 'https://www.jusbrasil.com.br/artigos/multa-dos-40-do-fgts-no-contrato-de-trabalho-temporario/2133490920' },
+    { id: 'botao_salario', url: 'https://www.jusbrasil.com.br/artigos/consequencias-pelo-traso-no-pagamento-do-salario/2130555285?_gl=1*1yzo4ba*_ga*MzYxNzg5NTM1LjE3MDk0MjU5MDk.*_ga_QCSXBQ8XPZ*MTcxMDAyMzY5MC4zLjEuMTcxMDAyNDkwMC4xMS4wLjA.' },
+    { id: 'social1', url: 'https://www.instagram.com/advgusttavonogueira/' },
+    { id: 'social2', url: 'https://w.app/c4S3QE' },
+    { id: 'conhecer_mais', url: 'https://w.app/c4S3QE' },
+    { id: 'entrar_contato', url: 'https://w.app/c4S3QE' },
+];
+
+urls.forEach(par => {
+    const botao = document.getElementById(par.id);
+    botao.addEventListener('click', function() {
+        window.open(par.url, '_blank');
+    });
+});
+
 const linkBotaoTrabalhos = document.querySelectorAll('#botao_trabalhos');
-
-linkBotaoSocilInsta.addEventListener('click', function () {
-    window.open('https://www.instagram.com/advgusttavonogueira/', '_blank')
-});
-
-linkBotaoSocialWhats.addEventListener('click', function () {
-    window.open('https://w.app/c4S3QE', '_blank')
-});
-
-linkBotaoFgts.addEventListener('click', function () {
-    window.open('https://www.jusbrasil.com.br/artigos/multa-dos-40-do-fgts-no-contrato-de-trabalho-temporario/2133490920', '_blank')
-});
-
-linkBotaoSalario.addEventListener('click', function () {
-    window.open('https://www.jusbrasil.com.br/artigos/consequencias-pelo-traso-no-pagamento-do-salario/2130555285?_gl=1*1yzo4ba*_ga*MzYxNzg5NTM1LjE3MDk0MjU5MDk.*_ga_QCSXBQ8XPZ*MTcxMDAyMzY5MC4zLjEuMTcxMDAyNDkwMC4xMS4wLjA.', '_blank')
-});
-
 linkBotaoTrabalhos.forEach(botao => {
-    botao.addEventListener('click', function () {
-        window.open('https://w.app/c4S3QE', '_blank')
+    botao.addEventListener('click', function() {
+        window.open('https://w.app/c4S3QE', '_blank');
     });
 });
 
@@ -53,30 +48,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // LI FICAREM SELECIONADA COM O HOVER
 
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("header nav ul li a");
+function setActiveMenuItem() {
+    const sections = document.querySelectorAll('section');
 
-    function highlightNavItem() {
-        sections.forEach(section => {
-            const rect = section.getBoundingClientRect();
-            const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
+    sections.forEach(section => {
+        const sectionId = section.getAttribute('id');
 
-            if (isInViewport) {
-                const sectionId = section.getAttribute("id");
-                navLinks.forEach(link => {
-                    if (link.getAttribute("href") === `#${sectionId}`) {
-                        link.classList.add("active");
-                    } else {
-                        link.classList.remove("active");
-                    }
-                });
-            }
+        const menuItem = document.querySelector(`nav a[href="#${sectionId}"]`);
+
+        const sectionIsInView = section.getBoundingClientRect().top <= 50 && section.getBoundingClientRect().bottom >= 50;
+
+        document.querySelectorAll('nav a').forEach(item => {
+            item.classList.remove('active');
         });
-    }
 
-    window.addEventListener("scroll", highlightNavItem);
-});
+        if (sectionIsInView) {
+            menuItem.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('load', setActiveMenuItem);
+window.addEventListener('scroll', setActiveMenuItem);
 
 // MENU hamburguer
 
@@ -84,22 +77,17 @@ const hamburguer = document.querySelector('.hamburguer');
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 const menuItems = document.querySelectorAll('.menu-hamburguer li a');
 
-// Evento de clique no ícone de hambúrguer
+
 hamburguer.addEventListener('click', () => {
     menuHamburguer.style.display = 'block';
 });
 
-// Evento de clique em qualquer parte do menu de hambúrguer
 menuHamburguer.addEventListener('click', (event) => {
-    // Verifica se o clique ocorreu em um item do menu
     if (event.target.tagName === 'A') {
-        // Remove a classe ativa de todos os itens do menu
         menuItems.forEach(item => {
             item.classList.remove('active');
         });
-        // Adiciona a classe ativa apenas ao item clicado
         event.target.classList.add('active');
-        // Fecha o menu de hambúrguer
         menuHamburguer.style.display = 'none';
     }
 });
